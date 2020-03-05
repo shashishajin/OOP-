61571c32ea6af141c2c1f43761e9b7c2eb0d3787
jil_youg <susu2721404@gmail.com>
2020-03-06 00:11:11 +0800

    Nothing

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -1,8 +1,14 @@
-﻿#include "pch.h"
+﻿#include "stdafx.h"
 #include <string>
 #include <iostream>
 using namespace System;
 bool Guess(int number) {
+	static int target = -1;
+	if (target == -1) {
+		Random r;
+		target = r.Next() % 100 + 1;
+	}
+	if (number == target)return true;
 	return false;
 }
 int main(array<System::String ^> ^args)
